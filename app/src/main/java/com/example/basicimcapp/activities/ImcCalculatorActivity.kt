2f599826e60d7f90465df97005efd282a2ca9dde
Ivan Espinosa <ivarespar@gmail.com>
2023-10-09 03:13:29 -0500

    [o9] Crear UI: hombre - mujer xml

---
+++ app/src/main/java/com/example/basicimcapp/activities/ImcCalculatorActivity.kt
@@ -0,0 +1,12 @@
+package com.example.basicimcapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.basicimcapp.R
+
+class ImcCalculatorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_imc_calculator)
+    }
+}
